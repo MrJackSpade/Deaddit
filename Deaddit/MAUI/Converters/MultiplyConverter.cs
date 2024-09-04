d4b1ed4df1fd675b855550028466ab79a1f9e53e
MrJackSpade <loffredo.ap@gmail.com>
2024-09-04 02:22:30 -0700

    no message

---
+++ Deaddit/MAUI/Converters/MultiplyConverter.cs
@@ -0,0 +1,28 @@
+﻿using System.Globalization;
+
+namespace Deaddit.MAUI.Converters
+{
+
+    public class MultiplyConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is double doubleValue && parameter is string factorString && double.TryParse(factorString, out double factor))
+            {
+                return doubleValue * factor;
+            }
+
+            return value;  // Return the original value if there's any issue
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is double doubleValue && parameter is string factorString && double.TryParse(factorString, out double factor))
+            {
+                return doubleValue / factor;
+            }
+
+            return value;
+        }
+    }
+}
